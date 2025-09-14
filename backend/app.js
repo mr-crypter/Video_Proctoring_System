34a084c94e865c738034dab83ab014f1820b90ef
mr-crypter <omsaxena8426@gmail.com>
2025-09-14 20:15:20 +0530

    deploy errorfixed

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const corsOptions = {
   credentials: true,
 };
 
-app.options('*', cors(corsOptions));
+app.options('(.*)', cors(corsOptions));
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
